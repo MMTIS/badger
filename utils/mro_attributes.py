448b2b0081107650790dca343d113f264073cd21
Stefan de Konink <stefan@konink.de>
2025-03-12 21:14:54 +0100

    MyPy

--- utils/mro_attributes.py
+++ utils/mro_attributes.py
@@ -1,6 +1,8 @@
 import dataclasses
 import decimal
 from dataclasses import Field
+from enum import Enum
+from typing import Any
 
 from xsdata.models.datatype import XmlDateTime, XmlDuration, XmlTime, XmlDate, XmlPeriod
 
@@ -8,21 +10,30 @@ import netex
 import inspect
 import typing
 
+T = typing.TypeVar("T")
 
-def hasdefault(default):
-    if isinstance(default, dataclasses._MISSING_TYPE):
+# TODO: This class is currently only used by projection, it seems that it is correctly working. It would be good to do some cleanups and tests on it.
+
+
+def hasdefault(field: dataclasses.Field[typing.Any]) -> typing.Any:
+    if isinstance(field.default, dataclasses._MISSING_TYPE):
         return None
 
-    return default
+    return field.default
 
 
-def unembed(fields):
+def unembed(
+    fields: dict[str, dataclasses.Field[typing.Any]]
+) -> typing.Iterable[tuple[str, dataclasses.Field[typing.Any]]]:
     all_references = []
     all_classes = []
 
     for name, field in fields.items():
+        if isinstance(field.type, str):
+            continue
+
         resolved_class = resolve_class(field.type)
-        if not resolved_class:
+        if not resolved_class or not isinstance(resolved_class, type):
             continue
 
         if hasattr(resolved_class, "__forward_arg__"):
@@ -30,27 +41,23 @@ def unembed(fields):
 
         resolved_class_name = resolved_class.__mro__[0].__name__
 
-        if resolved_class_name.endswith('Ref'):
+        if resolved_class_name.endswith("Ref"):
             all_references.append(resolved_class_name[:-3])
 
         all_classes.append((name, field, resolved_class_name))
 
-    result = []
     for name, field, resolved_class_name in all_classes:
         if resolved_class_name not in all_references:
-            result.append((name, field))
-
-    return result
+            yield name, field
 
 
-def resolve_class(clazz):
+def resolve_class(clazz: typing.Type[typing.Any]) -> typing.Type[typing.Any] | None:
     clazz_resolved = clazz
 
-    if hasattr(clazz, '_name'):
-        if clazz._name == 'Optional':
-            optional = True
+    if hasattr(clazz, "_name"):
+        if clazz._name == "Optional":
             clazz_resolved = [x for x in clazz.__args__ if x is not None.__class__][0]
-        elif clazz._name == 'List':
+        elif clazz._name == "List":
             return None  # TODO: handle list elements
         else:
             clazz_resolved = [x for x in clazz.__args__ if x is not None.__class__][0]
@@ -58,29 +65,29 @@ def resolve_class(clazz):
     return clazz_resolved
 
 
-IGNORE_ATTRIBUTES = ['name_of_class_attribute']
+IGNORE_ATTRIBUTES = ["name_of_class_attribute"]
 
 
-def list_attributes(clazz, parent_name=None):
-    buffer = []
-    if hasattr(clazz, '__dataclass_fields__'):
+def list_attributes(
+    clazz: T, parent_name: str | None = None
+) -> typing.Iterable[tuple[str, tuple[type[Any], bool] | None, Any, Field[Any]]]:
+    if hasattr(clazz, "__dataclass_fields__"):
         for name, field in unembed(clazz.__dataclass_fields__):
-            if name in IGNORE_ATTRIBUTES:
+            if name in IGNORE_ATTRIBUTES or isinstance(field.type, str):
                 continue
 
             if parent_name:
-                full_name = parent_name + '.' + name
-                buffer.append((full_name, get_type(field.type, full_name), hasdefault(field.default), field))
+                full_name = parent_name + "." + name
+                yield full_name, get_type(field.type, full_name), hasdefault(
+                    field
+                ), field
             else:
-                buffer.append((name, get_type(field.type, name), hasdefault(field.default), field))
-                # return (name, get_type(field.type, name))
-
-    return buffer
+                yield name, get_type(field.type, name), hasdefault(field), field
 
 
-def likely_type(obj: Field):
-    if hasattr(obj.type, '__args__'):
-        if hasattr(obj.type.__args__[0], '__name__'):
+def likely_type(obj: Field[typing.Any]) -> typing.Any:
+    if hasattr(obj.type, "__args__"):
+        if hasattr(obj.type.__args__[0], "__name__"):
             return obj.type.__args__[0].__name__
 
         elif obj.type.__args__[0].__class__ == typing.ForwardRef:
@@ -88,13 +95,17 @@ def likely_type(obj: Field):
 
         else:
             return obj.type.__class__.__name__
-    elif obj.type.__class__.__name__ == 'type':
+    elif isinstance(
+        obj.type, type
+    ):  # TODO: verify is the same obj.type.__class__.__name__ == 'type':
         return obj.type.__name__
     else:
         return obj.type.__class__.__name__
 
 
-def get_type(clazz, parent_name):
+def get_type(
+    clazz: type[typing.Any], parent_name: str
+) -> tuple[typing.Type[typing.Any], bool] | None:
     optional = False
     clazz_resolved = clazz
 
@@ -102,20 +113,20 @@ def get_type(clazz, parent_name):
         # We don't handle these yet
         return None
 
-    if hasattr(clazz, '_name'):
-        if clazz._name == 'Optional':
+    if hasattr(clazz, "_name"):
+        if clazz._name == "Optional":
             optional = True
             clazz_resolved = [x for x in clazz.__args__ if x is not None.__class__][0]
-        elif clazz._name == 'List':
+        elif clazz._name == "List":
             return None  # TODO: handle list elements
         else:
             clazz_resolved = [x for x in clazz.__args__ if x is not None.__class__][0]
 
-    if clazz_resolved == object:
+    if isinstance(clazz_resolved, object):  # TODO: check this
         # TODO: We don't handle these yet
         return None
 
-    if hasattr(clazz_resolved, "_name") and clazz_resolved._name == 'List':
+    if hasattr(clazz_resolved, "_name") and clazz_resolved._name == "List":
         # TODO: We don't handle these yet
         return None
 
@@ -127,21 +138,38 @@ def get_type(clazz, parent_name):
         print(parent_name, clazz_resolved)
         print()
 
-    if len([x for x in clazz_resolved.__mro__ if x.__name__.endswith('RelStructure')]) > 0:
+    if (
+        len([x for x in clazz_resolved.__mro__ if x.__name__.endswith("RelStructure")])
+        > 0
+    ):
         return None
 
-    if len([x for x in clazz_resolved.__mro__ if x.__name__ == 'Enum']) > 0:
+    if len([x for x in clazz_resolved.__mro__ if x.__name__ == "Enum"]) > 0:
         return str, optional  # (get_enum(clazz_resolved), optional)
 
     # This is a hack because upstream did not use VersionOfObjectRef as substitution group consistently
-    if len([x for x in clazz_resolved.__mro__ if x.__name__.endswith('RefStructure')]) > 0:
+    if (
+        len([x for x in clazz_resolved.__mro__ if x.__name__.endswith("RefStructure")])
+        > 0
+    ):
         # Inline RefClasses
         # return (list_attributes(clazz_resolved, parent_name), optional)
         return netex.VersionOfObjectRef, optional
 
     if clazz_resolved not in (
-            int, str, bool, float, bytes, XmlPeriod, XmlTime, XmlDate, XmlDateTime, XmlDuration, decimal.Decimal,
-            netex.MultilingualString):
+        int,
+        str,
+        bool,
+        float,
+        bytes,
+        XmlPeriod,
+        XmlTime,
+        XmlDate,
+        XmlDateTime,
+        XmlDuration,
+        decimal.Decimal,
+        netex.MultilingualString,
+    ):
         listed_attributes = list_attributes(clazz_resolved, parent_name)
         if listed_attributes:
             return listed_attributes, optional
@@ -150,5 +178,5 @@ def get_type(clazz, parent_name):
     return clazz_resolved, optional
 
 
-def get_enum(clazz):
+def get_enum(clazz: type[Enum]) -> list[str]:
     return [x[1].value for x in inspect.getmembers(clazz) if isinstance(x[1], clazz)]
