ea0cc6dfd246bbd09db02e6af0b04c7dedac1d2f
Stefan de Konink <stefan@konink.de>
2025-03-13 09:46:29 +0100

    MyPy

--- utils/netex_monkeypatching.py
+++ utils/netex_monkeypatching.py
@@ -1,80 +1,132 @@
-from netex import VersionOfObjectRefStructure, ServiceJourneyRef, ServiceJourneyPatternRef, ServiceJourney, \
-    ServiceJourneyPattern, RouteRefStructure, RouteLinkRefStructure, ScheduledStopPointRefStructure, ScheduledStopPoint, \
-    RouteLink, Route, Quay, QuayRefStructure, StopPlaceRefStructure, StopPlace, TimingPoint, TimingPointRefStructure, \
-    ServiceLink, ServiceLinkRefStructure, TimingLink, TimingLinkRefStructure, Locale, LocaleStructure, OperatorRef, \
-    DayTypeRef, DayTypesInFrameRelStructure, DayTypeRefsRelStructure, ValidityConditionsRelStructure, \
-    UicOperatingPeriodRef, OperatingPeriodRef, LineRef, QuayRef, ScheduledStopPointRef
+from netex import (
+    VersionOfObjectRefStructure,
+    ServiceJourneyRef,
+    ServiceJourneyPatternRef,
+    ServiceJourney,
+    ServiceJourneyPattern,
+    Locale,
+    LocaleStructure,
+    OperatorRef,
+    DayTypeRef,
+    DayTypeRefsRelStructure,
+    ValidityConditionsRelStructure,
+    UicOperatingPeriodRef,
+    OperatingPeriodRef,
+    LineRef,
+    QuayRef,
+    ScheduledStopPointRef,
+    EntityInVersion,
+)
 
-def ref_version_hash(self):
-    return hash(self.ref + ';' + self.version)
 
-VersionOfObjectRefStructure.__hash__ = ref_version_hash
-ServiceJourneyRef.__hash__ = ref_version_hash
-ServiceJourneyPatternRef.__hash__ = ref_version_hash
-OperatorRef.__hash__ = ref_version_hash
-DayTypeRef.__hash__ = ref_version_hash
-OperatingPeriodRef.__hash__ = ref_version_hash
-UicOperatingPeriodRef.__hash__ = ref_version_hash
-LineRef.__hash__ = ref_version_hash
-QuayRef.__hash__ = ref_version_hash
-ScheduledStopPointRef.__hash__ = ref_version_hash
+def ref_version_hash(self: VersionOfObjectRefStructure) -> int:
+    assert self.ref is None
+    return hash(self.ref + ";" + self.version or "any")
+
+
+setattr(VersionOfObjectRefStructure, ".__hash__", ref_version_hash)
+setattr(ServiceJourneyRef, "ref_version_hash", ref_version_hash)
+setattr(ServiceJourneyPatternRef, "ref_version_hash", ref_version_hash)
+setattr(OperatorRef, "ref_version_hash", ref_version_hash)
+setattr(DayTypeRef, "ref_version_hash", ref_version_hash)
+setattr(OperatingPeriodRef, "ref_version_hash", ref_version_hash)
+setattr(UicOperatingPeriodRef, "ref_version_hash", ref_version_hash)
+setattr(LineRef, "ref_version_hash", ref_version_hash)
+setattr(QuayRef, "ref_version_hash", ref_version_hash)
+setattr(ScheduledStopPointRef, "ref_version_hash", ref_version_hash)
+
 
 # TODO: the following hashes us version or 'any' to overcome (invalid) None situations, it would be better if we could create hashes that would capture the true NeTEx-any situation
-def day_type_refs_hash(self: DayTypeRefsRelStructure):
-    return hash('\n'.join([dtr.ref + ';' + (dtr.version or 'any') for dtr in self.day_type_ref]))
+def day_type_refs_hash(self: DayTypeRefsRelStructure) -> int:
+    return hash(
+        "\n".join([dtr.ref + ";" + (dtr.version or "any") for dtr in self.day_type_ref])
+    )
 
-DayTypeRefsRelStructure.__hash__ = day_type_refs_hash
 
-def vc_refs_hash(self: ValidityConditionsRelStructure):
+setattr(DayTypeRefsRelStructure, "day_type_refs_hash", ref_version_hash)
+
+
+def vc_refs_hash(self: ValidityConditionsRelStructure) -> int:
     refs = []
     for vc in self.choice:
-        if hasattr(vc, 'id'):
-            refs.append(vc.id + ';' + (vc.version or 'any'))
-        elif hasattr(vc, 'ref'):
-            refs.append(vc.ref + ';' + (vc.version or 'any'))
+        if hasattr(vc, "id"):
+            assert vc.id is not None
+            refs.append(vc.id + ";" + (vc.version or "any"))
+        elif hasattr(vc, "ref"):
+            assert vc.ref is not None
+            refs.append(vc.ref + ";" + (vc.version or "any"))
 
-    return hash('\n'.join(refs))
-
-ValidityConditionsRelStructure.__hash__ = vc_refs_hash
+    return hash("\n".join(refs))
 
 
-def id_version_hash(self):
-    return hash(self.id + ';' + (self.version or 'any'))
+setattr(ValidityConditionsRelStructure, "vc_refs_hash", ref_version_hash)
 
-ServiceJourney.__hash__ = id_version_hash
-ServiceJourneyPattern.__hash__ = id_version_hash
 
+def id_version_hash(self: EntityInVersion) -> int:
+    assert self.id is not None and self.version is not None
+    return hash(self.id + ";" + (self.version or "any"))
+
+
+setattr(ServiceJourney, "id_version_hash", ref_version_hash)
+setattr(ServiceJourneyPattern, "id_version_hash", ref_version_hash)
+
+
+def hash_locale(self: Locale) -> int:
+    return hash(
+        (
+            self.time_zone_offset,
+            self.time_zone,
+            self.summer_time_zone_offset,
+            self.summer_time_zone,
+            self.default_language,
+        )
+    )
+
+
+setattr(Locale, "hash_locale", ref_version_hash)
+setattr(LocaleStructure, "hash_locale", ref_version_hash)
+
+"""
 # First monkey patching test
 def get_route(self, con) -> Route:
     return con.get_single(Route, self.ref, self.version)
 
-RouteRefStructure.get = get_route
+
+setattr(RouteRefStructure, "get_route", ref_version_hash)
+
 
 def get_routelink(self, con) -> RouteLink:
     return con.get_single(RouteLink, self.ref, self.version)
 
-RouteLinkRefStructure.get = get_routelink
+
+setattr(RouteLinkRefStructure, "get_routelink", ref_version_hash)
+
 
 def get_scheduledstoppoint(self, con) -> ScheduledStopPoint:
     return con.get_single(ScheduledStopPoint, self.ref, self.version)
 
-ScheduledStopPointRefStructure.get = get_scheduledstoppoint
+
+setattr(ScheduledStopPointRefStructure, "get_scheduledstoppoint", ref_version_hash)
+
 
 def get_quay(self, con) -> Quay:
     return con.get_single(Quay, self.ref, self.version)
 
-QuayRefStructure.get = get_quay
+
+setattr(QuayRefStructure, "get_quay", ref_version_hash)
+
 
 def get_stopplace(self, con) -> StopPlace:
     return con.get_single(StopPlace, self.ref, self.version)
 
-StopPlaceRefStructure.get = get_stopplace
+
+setattr(StopPlaceRefStructure, "get_stopplace", ref_version_hash)
 
 
 def get_timingpoint(self, con) -> TimingPoint | ScheduledStopPoint:
     if self.name_of_ref_class == 'TimingPoint':
         return con.get_single(TimingPoint, self.ref, self.version)
-    elif  self.name_of_ref_class == 'ScheduledStopPoint':
+    elif self.name_of_ref_class == 'ScheduledStopPoint':
         return con.get_single(ScheduledStopPoint, self.ref, self.version)
     else:
         timing_point = con.get_single(TimingPoint, self.ref, self.version)
@@ -83,20 +135,20 @@ def get_timingpoint(self, con) -> TimingPoint | ScheduledStopPoint:
         else:
             return con.get_single(ScheduledStopPoint, self.ref, self.version)
 
-TimingPointRefStructure.get = get_timingpoint
+
+setattr(TimingPointRefStructure, "get_timingpoint", ref_version_hash)
+
 
 def get_servicelink(self, con) -> ServiceLink:
     return con.get_single(ServiceLink, self.ref, self.version)
 
-ServiceLinkRefStructure.get = get_servicelink
+
+setattr(ServiceLinkRefStructure, "get_servicelink", ref_version_hash)
+
 
 def get_timinglink(self, con) -> TimingLink:
     return con.get_single(TimingLink, self.ref, self.version)
 
-TimingLinkRefStructure.get = get_timinglink
 
-def hash_locale(self):
-    return hash((self.time_zone_offset, self.time_zone, self.summer_time_zone_offset, self.summer_time_zone, self.default_language,))
-
-Locale.__hash__ = hash_locale
-LocaleStructure.__hash__ = hash_locale
+setattr(TimingLinkRefStructure, "get_timinglink", ref_version_hash)
+"""
