05b4a4df92cc6edfb5fe0d38a4a29fe13448fbcb
Stefan de Konink <stefan@konink.de>
2025-09-26 13:13:29 +0200

    Generate separate xsData files for development

--- scripts/generate-schema.sh
+++ scripts/generate-schema.sh
@@ -4,4 +4,5 @@ echo "📦 Updating the git submodules..."
 git submodule update --remote
 
 echo "⚙️  Generating the Python-code from the XML Schema..."
-uv run xsdata generate -c domain/netex/conf/single.conf domain/netex/schema/xsd/NeTEx_publication.xsd
+# uv run xsdata generate -c domain/netex/conf/single.conf domain/netex/schema/xsd/NeTEx_publication.xsd
+uv run xsdata generate -c domain/netex/conf/xsdata.conf domain/netex/schema/xsd/NeTEx_publication.xsd
