136c7a8d7bca64e9df9acd0d10391216f712d337
Stefan de Konink <stefan@konink.de>
2025-12-16 16:40:47 +0100

    Fix #68

--- transformers/ivu.py
+++ transformers/ivu.py
@@ -2,14 +2,29 @@ from typing import Generator
 
 from mdbx.mdbx import TXN
 
-from domain.netex.model import Line, ServiceJourneyPattern, ServiceJourney, Route, RouteRef, RouteView, LineRef, \
-    FlexibleLineRef, OperatorRef, ServiceJourneyPatternRef, VehicleType, MultilingualString, StopPlace, Quay
+from domain.netex.model import (
+    Line,
+    ServiceJourneyPattern,
+    ServiceJourney,
+    Route,
+    RouteRef,
+    RouteView,
+    LineRef,
+    FlexibleLineRef,
+    OperatorRef,
+    ServiceJourneyPatternRef,
+    VehicleType,
+    MultilingualString,
+    StopPlace,
+    Quay,
+)
+import re
 from storage.mdbx.core.implementation import MdbxStorage
 
 
 def avv_service_journey_operator(db_read: MdbxStorage, txn: TXN) -> Generator[ServiceJourney, None, None]:
-    line_operator_ref = {line.id : line.operator_ref for line in db_read.iter_only_objects(txn, Line)}
-    route_operator_ref = {route.id : line_operator_ref[route.line_ref.ref] for route in db_read.iter_only_objects(txn, Route) if route.line_ref}
+    line_operator_ref = {line.id: line.operator_ref for line in db_read.iter_only_objects(txn, Line)}
+    route_operator_ref = {route.id: line_operator_ref[route.line_ref.ref] for route in db_read.iter_only_objects(txn, Route) if route.line_ref}
 
     sjp_operator_ref = {}
     sjp: ServiceJourneyPattern
@@ -26,24 +41,24 @@ def avv_service_journey_operator(db_read: MdbxStorage, txn: TXN) -> Generator[Se
 
     sj: ServiceJourney
     for sj in db_read.iter_only_objects(txn, ServiceJourney):
-        if not isinstance(sj.operator_ref_or_operator_view_or_passenger_carrying_requirement_ref_or_passenger_carrying_requirements_view, OperatorRef):
+        if not isinstance(sj.operator_ref_or_operator_view, OperatorRef):
             if isinstance(sj.flexible_line_ref_or_line_ref_or_line_view_or_flexible_line_view, LineRef):
-                sj.operator_ref_or_operator_view_or_passenger_carrying_requirement_ref_or_passenger_carrying_requirements_view = line_operator_ref[sj.flexible_line_ref_or_line_ref_or_line_view_or_flexible_line_view.ref]
+                sj.operator_ref_or_operator_view = line_operator_ref[sj.flexible_line_ref_or_line_ref_or_line_view_or_flexible_line_view.ref]
 
             elif isinstance(sj.route_ref, RouteRef):
-                sj.operator_ref_or_operator_view_or_passenger_carrying_requirement_ref_or_passenger_carrying_requirements_view = route_operator_ref[sj.route_ref.ref]
+                sj.operator_ref_or_operator_view = route_operator_ref[sj.route_ref.ref]
 
             elif isinstance(sj.journey_pattern_ref, ServiceJourneyPatternRef):
-                sj.operator_ref_or_operator_view_or_passenger_carrying_requirement_ref_or_passenger_carrying_requirements_view = sjp_operator_ref[sj.journey_pattern_ref.ref]
+                sj.operator_ref_or_operator_view = sjp_operator_ref[sj.journey_pattern_ref.ref]
 
         yield sj
 
-import re
 
 def abbreviate_initials(text: str, max_len: int = 5) -> str:
     words = re.findall(r"[A-Za-z0-9]+", text)
     initials = "".join(w[0] for w in words)
     return initials[:max_len] if len(initials) > max_len else initials
+
 
 def abbreviate_hybrid(text: str, max_len: int = 5) -> str:
     words = re.findall(r"[A-Za-z0-9]+", text)
@@ -61,6 +76,7 @@ def abbreviate_hybrid(text: str, max_len: int = 5) -> str:
 
     return str(abb.upper())
 
+
 def make_abbreviation(text: str, max_len: int = 5) -> str:
     text = text.strip()
 
@@ -73,12 +89,14 @@ def make_abbreviation(text: str, max_len: int = 5) -> str:
 
     return abb.upper()
 
+
 def avv_vehicle_type_short_name(db_read: MdbxStorage, txn: TXN) -> Generator[VehicleType, None, None]:
     vt: VehicleType
     for vt in db_read.iter_only_objects(txn, VehicleType):
         if vt.name:
             vt.short_name = MultilingualString(content=[make_abbreviation(str(vt.name.content[0]), 5)])
             yield vt
+
 
 def avv_quay_name(db_read: MdbxStorage, txn: TXN) -> Generator[StopPlace, None, None]:
     sp: StopPlace
