bae480fe5d285ce0cb0ae943eabe8338d4a9a5ff
Matthias Günter <matthias.guenter@sbb.ch>
2026-01-17 00:41:57 +0100

    really saving direction when automatically generated
    When we automatically generate  direction, we need to store it. reorganised the yielding.

--- transformers/direction.py
+++ transformers/direction.py
@@ -1,4 +1,4 @@
-from typing import Dict, Any, Generator, cast
+from typing import Dict, Any, Generator, cast, Tuple, Optional
 
 from mdbx.mdbx import TXN
 from domain.netex.model import ServiceJourneyPattern, Direction, MultilingualString, DirectionRef, DirectionType
@@ -13,29 +13,36 @@ def infer_directions_from_sjps_and_apply(db_read: MdbxStorage, txn: TXN, generat
     directions: Dict[str, Direction] = {}
     direction_refs: Dict[str, DirectionRef | None] = {}
 
-    def process(sjp: ServiceJourneyPattern, generator_defaults: dict[str, Any]) -> ServiceJourneyPattern | None:
+    def process(sjp: ServiceJourneyPattern, generator_defaults: dict[str, Any]) -> Tuple[Optional[ServiceJourneyPattern], Optional[Direction]] :
+        # TODO we should perhaps think about, if we should invent Direction. It is not mandatory.
         if sjp.direction_type is not None and sjp.direction_ref_or_direction_view is None:
             key = str(sjp.direction_type.value)
             direction: Direction | None = directions.get(key, None)
+            new_direction=None
             if direction is None:
-                direction = Direction(
-                    id=getId(generator_defaults['codespace'], Direction, key),
-                    version='any',
-                    name=MultilingualString(content=[key]),
-                    direction_type=sjp.direction_type,
-                )
-                directions[key] = direction
+                if directions.get(key) is None:
+                    direction = Direction(
+                        id=getId(generator_defaults['codespace'], Direction, key),
+                        version='any',
+                        name=MultilingualString(content=[key]),
+                        direction_type=sjp.direction_type,
+                    )
+                    directions[key] = direction
+                    new_direction=direction
                 direction_refs[key] = cast(DirectionRef, getRef(direction))
             sjp.direction_ref_or_direction_view = direction_refs[key]
-            return sjp
+            return sjp, new_direction
 
-        return None
+        return None, None
 
     def query(db_read: MdbxStorage) -> Generator[ServiceJourneyPattern, None, None]:
         for sjp in db_read.iter_only_objects(txn, ServiceJourneyPattern):
-            new_sjp = process(sjp, generator_defaults)
+            new_sjp,new_direction = process(sjp, generator_defaults)
+            if new_direction is not None:
+                yield new_direction
             if new_sjp is not None:
                 yield new_sjp
 
+
     yield from query(db_read)
-    yield from directions.values()
+
