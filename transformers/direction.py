d8b82d9b99fdf20a6d422e8a97661725f4315f04
Matthias Günter <matthias.guenter@sbb.ch>
2026-01-17 12:39:24 +0100

    stefan: everything with yield now

--- transformers/direction.py
+++ transformers/direction.py
@@ -13,7 +13,7 @@ def infer_directions_from_sjps_and_apply(db_read: MdbxStorage, txn: TXN, generat
     directions: Dict[str, Direction] = {}
     direction_refs: Dict[str, DirectionRef | None] = {}
 
-    def process(sjp: ServiceJourneyPattern, generator_defaults: dict[str, Any]) -> Tuple[Optional[ServiceJourneyPattern], Optional[Direction]] :
+    def process(sjp: ServiceJourneyPattern, generator_defaults: dict[str, Any]) -> Generator[ServiceJourneyPattern | Direction, None, None] :
         # TODO we should perhaps think about, if we should invent Direction. It is not mandatory.
         if sjp.direction_type is not None and sjp.direction_ref_or_direction_view is None:
             key = str(sjp.direction_type.value)
@@ -28,21 +28,14 @@ def infer_directions_from_sjps_and_apply(db_read: MdbxStorage, txn: TXN, generat
                         direction_type=sjp.direction_type,
                     )
                     directions[key] = direction
-                    new_direction=direction
+                    yield direction
                 direction_refs[key] = cast(DirectionRef, getRef(direction))
             sjp.direction_ref_or_direction_view = direction_refs[key]
-            return sjp, new_direction
+            yield sjp
 
-        return None, None
-
-    def query(db_read: MdbxStorage) -> Generator[ServiceJourneyPattern, None, None]:
+    def query(db_read: MdbxStorage) -> Generator[ServiceJourneyPattern | Direction, None, None]:
         for sjp in db_read.iter_only_objects(txn, ServiceJourneyPattern):
-            new_sjp,new_direction = process(sjp, generator_defaults)
-            if new_direction is not None:
-                yield new_direction
-            if new_sjp is not None:
-                yield new_sjp
-
+            yield from process(sjp, generator_defaults)
 
     yield from query(db_read)
 
