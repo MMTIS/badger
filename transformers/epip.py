e42cdab86ffa3be519208856b28d77224227c0dd
Stefan de Konink <stefan@konink.de>
2026-01-26 14:55:23 +0100

    Fix #97

--- transformers/epip.py
+++ transformers/epip.py
@@ -613,9 +613,9 @@ def epip_service_journey_generator(db_read: MdbxStorage, txn: TXN, generator_def
                 service_journey_pattern = service_journey_pattern_from_calls(sj, generator_defaults)
                 sj.journey_pattern_ref = getRef(service_journey_pattern)
 
-            # sj.passing_times = TimetabledPassingTimesRelStructure(
-            #     timetabled_passing_time=TimetablePassingTimesProfile.getTimetabledPassingtimesFromCalls(sj, service_journey_pattern)
-            # )
+            sj.passing_times = TimetabledPassingTimesRelStructure(
+                 timetabled_passing_time=TimetablePassingTimesProfile.getTimetabledPassingtimesFromCalls(sj, service_journey_pattern)
+            )
 
         elif sj.journey_pattern_ref and sj.time_demand_type_ref:
             service_journey_pattern: ServiceJourneyPattern = db_read.load_object_by_reference(txn, sj.journey_pattern_ref)
