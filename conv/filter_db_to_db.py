0b52feab8ab7955a2051d7d51dec27dc580c2652
Matthias Günter <matthias.guenter@sbb.ch>
2026-08-03 21:20:44 +0200

    Update filter_db_to_db.py

--- conv/filter_db_to_db.py
+++ conv/filter_db_to_db.py
@@ -410,7 +410,7 @@ if __name__ == "__main__":
     )
 
     parser.add_argument("--log_file", type=str, required=False, help="the logfile")
-    args, kwargs = parser.parse_args()
+    args = parser.parse_args()
     mylogger = prepare_logger(logging.INFO, args.log_file)
 
     try:
